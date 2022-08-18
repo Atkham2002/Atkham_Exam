95103a1ddc36f5f9d429cfedc7995e32c428cf50
Atkham <108746128+Atkham2002@users.noreply.github.com>
2022-08-18 11:46:15 +0500

    Update Controller.java

--- src/main/java/Atkham_Group/Exam/Controller/Controller.java
+++ src/main/java/Atkham_Group/Exam/Controller/Controller.java
@@ -3,6 +3,8 @@ package Atkham_Group.Exam.Controller;
 import Atkham_Group.Exam.dto.BookDto;
 import Atkham_Group.Exam.dto.ResponseDto;
 import Atkham_Group.Exam.service.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,8 @@ public class Controller {
         this.service = service;
     }
 
-    public ResponseDto<String> add(BookDto bookDto){
+    @PostMapping
+    public ResponseDto<String> add(@RequestBody BookDto bookDto){
         return service.addBook(bookDto);
     }
 
